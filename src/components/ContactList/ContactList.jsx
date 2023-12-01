f4061b933a1c73dd594e60fc59ca4c34f2d97717
Marixa82 <121823496+Marixa82@users.noreply.github.com>
2023-12-01 17:10:58 +0200

    add createSelector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,24 +1,14 @@
 import { Ul } from './ContactList.styled';
 import { ContactListItem } from "./ContactListItem";
 import { useSelector} from 'react-redux';
-import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
 
 export const ContactList = () => {
-    const items = useSelector(selectContacts);
-    const filter = useSelector(selectFilter);
-    const getVisibleContacts = () => {
-   const filteredContacts = items.filter((item) =>
-    item.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  return filteredContacts;
-  
-};
-    
+     const getVisibleContacts = useSelector(selectVisibleContacts);
     if(getVisibleContacts ===0) return null
     return (
         <Ul>
-            {getVisibleContacts().map(item => {
+            {getVisibleContacts.map(item => {
                 const { id } = item
                 return (
                     < ContactListItem
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,9 +1,16 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const selectContacts = state => state.contacts.contacts.items;
 export const selectIsLoading = state => state.contacts.contacts.isLoading;
 export const selectError = state => state.contacts.contacts.error;
 export const selectFilter = state => state.contacts.filter;
 
-
+export const selectVisibleContacts = createSelector(
+    [selectContacts, selectFilter], (items, filter) => {
+        return items.filter(contact => contact.name.toLowerCase()
+            .includes(filter.toLowerCase()))
+    }
+)
 
 
 
